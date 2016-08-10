25d1b0fc62ca55e77fe14a9d3da2124a5cd6483f
naveenrana1309 <naveenrana1309@gmail.com>
2016-08-10 16:58:37 +0530

    update pod spec

---
+++ Example/Pods/Target Support Files/NRMirror/NRMirror-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double NRMirrorVersionNumber;
+FOUNDATION_EXPORT const unsigned char NRMirrorVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-NRMirror_Tests/Pods-NRMirror_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NRMirror_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_NRMirror_Tests
+@end
